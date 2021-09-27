66c56e248aff1c673ab6343a36edbd680fd5da39
NAGARRO\eishtamittal <eishtamittal@gmail.com>
2021-09-27 19:54:27 +0530

    Observer Pattern- Pub/Sub

---
+++ 1.PubSub.js
@@ -0,0 +1,54 @@
+
+class PubSub {
+    constructor(){
+        this.observers = {};
+    }
+    // add the observer to listener
+    subscribe = (event, cb) => {
+        if (this.observers[event]) {
+            this.observers[event] = [...this.observers[event], cb];
+        }
+        else this.observers[event] = [cb];
+    }
+
+    // remove the observer
+    unsubscribe = (event) => {
+        let { [event]: a, ...rest } = this.observers;
+        this.observers = rest;
+    }
+
+    publish = (event, ...data) => {
+        this.observers[event]?.forEach(listener => {
+            listener.apply(this, data);
+        })
+    }
+}
+
+const pubSub = new PubSub();
+
+pubSub.subscribe("event1", (data) => {
+    console.log("First sub : Event1 data", data);
+});
+pubSub.subscribe("event1", (data) => {
+    console.log("Second Sub : Event1 data", data);
+});
+
+pubSub.subscribe("event2", (data) => {
+    console.log("Event2 data", data);
+});
+pubSub.publish("event1", { name: "IV1" });
+pubSub.publish("event2", { name: "IV2" });
+pubSub.unsubscribe("event1");
+pubSub.publish("event1", { name: "IV23" });
+
+
+// OUTPUT
+
+// First sub : Event1 data 
+// {name: "IV1"}
+// Second Sub : Event1 data 
+// {name: "IV1"}
+// Event2 data 
+// {name: "IV2"}
+
+
